19fb21850a6a2d854aa08b60ee68cec755686dd7
Jamar P <117375506+JamarPelzer@users.noreply.github.com>
2025-07-16 08:56:08 -0400

    Add more to math logic to app.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+## Math API logic Layer 
 print("Hello from DevOps CI/CD Lab")
-print("This is my first CI/Cd pipeline")
 
+def add(x, y):
+  return x + y
 
-def add(x,y):
-  return x + y 
+def subtract (x, y):
+  return x - y 
+
+def multiply(x, y):
+  return x * y 
+
+def divide (x, y):
+  if y == 0:
+    raise ValueError("Cannot divide by zero")
+  return x / y
+
+def exponent(x, y):
+  return x ** y 
+
+def modulo(x, y):
+  return x % y
